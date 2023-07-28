772888c43a20d69da1bf776bc0cac99a8ee2fb00
Miguel Menezes <limegu@gmail.com>
2023-07-28 11:28:33 -0300

    Solução 1 SEM exception

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,10 +1,39 @@
 package aplicacao;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+import model.entidades.Reserva;
+
 public class Programa {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws ParseException {
 
+		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+		System.out.print("Informe o número do quarto: ");
+		int nummeroQuarto = sc.nextInt();
+		System.out.print("Data de CHECK-IN (dd/mm/aaaa) : ");
+		Date checkin = sdf.parse(sc.next());
+		System.out.print("Data de CHECK-OUT (dd/mm/aaaa) : ");
+		Date checkout = sdf.parse(sc.next());
+
+		// A classe Date tem o método after, que testa se uma data é posterior a outra:
+		if (!checkout.after(checkin)) {
+
+			System.out.println("ERRO NA RESERVA! A data de check-OUT deve ser POSTERIOR à data de check-IN");
+
+		} else {
+
+			Reserva res = new Reserva(nummeroQuarto, checkin, checkout);
+			System.out.println(res);
+
+		}
+
+		sc.close();
 	}
 
 }
